ef23926842a69fac5283b51465041aeeecab6d3c
MazidulHasan <mazidulhasan1@gmail.com>
2024-12-21 00:51:24 +0600

    Frist Code With Avg Number Array

---
+++ Basic/AvgNumberInArray.py
@@ -0,0 +1,25 @@
+# Calculate the Average of Numbers in an Array
+#
+# 30. Calculate the Average of Numbers in an Array
+# Question: Given an array of numbers, calculate the average.
+# Hint: Sum all the numbers in the array and divide by the number of elements. Be mindful of potential division by zero if the array is empty.
+# Sample Input: [2, 4, 6, 8]
+# Sample Output: 5
+
+def calculateAVG(Array, length):
+    total = 0
+    for item in Array:
+        total += item
+
+    average = total // length
+    return average
+
+
+inArray = input("Enter elements: ")
+Array = list(map(int, inArray.split()))
+length = len(Array)
+
+if length == 0:
+    print("Array has no numbers.")
+else:
+    print(calculateAVG(Array, length))
